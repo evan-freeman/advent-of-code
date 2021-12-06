2dbf4c688d78fba40df7253b3206b96c1a94690f
Evan Freeman <59422750+evan-freeman@users.noreply.github.com>
2021-12-05 20:36:20 -0800

    more stuff

--- 2021/day4/day4.py
+++ 2021/day4/day4.py
@@ -1,6 +1,22 @@
+from itertools import product
+
+import numpy as np
+from numpy.core.fromnumeric import shape
+
 with open("2021/day4/input.txt") as f:
     input = f.read()
-print(input)
+# print(input)
 numbers, boards = input.split("\n\n", maxsplit=1)
-print(numbers)
-print(boards)
+numbers = [int(x) for x in numbers.split(",")]
+boards = boards.split("\n\n")
+board0 = boards[0]
+board0_list = board0.strip("/n").split()
+# board0_list = [int(x) for x in board0_list]
+
+combs = list(product(range(5), range(5)))
+board0 = np.array(board0_list, dtype=int).reshape(5, 5)
+print(board0)
+
+
+# print(board0_list)
+# print(combs)
