5a827baa3fa1b9c295b05e97a56442c51e2f7a27
Evan Freeman <59422750+evan-freeman@users.noreply.github.com>
2021-12-11 16:57:14 -0800

    done

--- 2021/day4/day4.py
+++ 2021/day4/day4.py
@@ -1,28 +1,76 @@
-from itertools import product
-
-import numpy as np
-
 with open("2021/day4/input.txt") as f:
     input = f.read()
-# print(input)
 numbers, boards = input.split("\n\n", maxsplit=1)
 numbers = [int(x) for x in numbers.split(",")]
 boards = boards.split("\n\n")
-board0 = boards[0]
-board0_list = board0.strip("/n").split()
-# board0_list = [int(x) for x in board0_list]
+boards = [board.split("\n") for board in boards]
+boards = [[[int(x) for x in row.split()] for row in board] for board in boards]
+board_list = []
+for board in boards:
+    board_list.append(
+        {
+            "cells": [
+                [{"num": num, "marked": 0} for num in row] for row in board
+            ],
+            "rows": [0, 0, 0, 0, 0],
+            "columns": [0, 0, 0, 0, 0],
+        }
+    )
+boards = {
+    "board_list": board_list,
+    "winning_numbers": [],
+    "winning_boards": [],
+}
 
-combs = list(product(range(5), range(5)))
-board0 = np.array(board0_list, dtype=int).reshape(5, 5)
-print(board0)
 
-# class Cell:
-#     def __init__(self):
-
-
-# class Board:
-#     def __init__(self):
+def bingo(numbers, boards):
+    # Do rounds of bingo
+    while numbers and boards["board_list"]:
+        number = numbers.pop(0)
+        boards = bingo_round(number, boards)
+    return boards["winning_numbers"], boards["winning_boards"]
 
 
-# print(board0_list)
-# print(combs)
+def bingo_round(number, boards):
+    # Mark all boards
+    # Remove winning boards
+    for board in boards["board_list"]:
+        mark_board(number, board)
+        if check_board(board):
+            boards["winning_numbers"].append(number)
+            boards["winning_boards"].append(board)
+            boards["board_list"].remove(board)
+    return boards
+
+
+def mark_board(number, board):
+    for i, row in enumerate(board["cells"]):
+        for j, cell in enumerate(row):
+            if cell["num"] == number and not cell["marked"]:
+                cell["marked"] = 1
+                board["rows"][i] += 1
+                board["columns"][j] += 1
+
+
+def check_board(board, size=5):
+    return size in board["rows"] + board["columns"]
+
+
+def score(number, board):
+    unmarked_numbers = [
+        cell["num"]
+        for row in board["cells"]
+        for cell in row
+        if not cell["marked"]
+    ]
+    return sum(unmarked_numbers) * number
+
+
+winning_numbers, winning_boards = bingo(numbers, boards)
+
+losing_number = winning_numbers[-1]
+losing_board = winning_boards[-1]
+
+print(losing_number)
+print(losing_board)
+print(score(losing_number, losing_board))
